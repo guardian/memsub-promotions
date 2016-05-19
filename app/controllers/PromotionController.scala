8865646fa02ae48f944b869684f82e43f17f4bb4
tverran <tom.verran@guardian.co.uk>
2016-05-19 16:22:11 +0100

    Replace client side promo filtering with server side filtering

--- app/controllers/PromotionController.scala
+++ app/controllers/PromotionController.scala
@@ -1,18 +1,28 @@
 package controllers
 
+import java.util.UUID
+import com.gu.memsub.promo._
 import com.gu.memsub.promo.Promotion.AnyPromotion
 import play.api.libs.concurrent.Execution.Implicits._
 import com.gu.memsub.services.JsonDynamoService
 import com.gu.memsub.promo.Formatters.PromotionFormatters._
+import com.gu.memsub.promo.Formatters.Common._
 import play.api.libs.json.{JsError, Json}
+import play.api.mvc.{Action, Result}
 import scala.concurrent.Future
 import play.api.mvc.Results._
-import play.api.mvc.Action
+import scala.util.Try
 
 class PromotionController(service: JsonDynamoService[AnyPromotion, Future]) {
 
-  def all = Action.async {
-    service.all.map(promos => Ok(Json.toJson(promos)))
+  def all(campaignCode: Option[String]) = Action.async {
+    campaignCode.map(CampaignCode).fold(service.all)(service.find).map(promos => Ok(Json.toJson(promos)))
+  }
+
+  def get(uuid: Option[String]) = Action.async {
+    uuid.flatMap(i => Try(UUID.fromString(i)).toOption).map {
+      i => service.find(i).map(_.headOption.fold[Result](NotFound)(promo => Ok(Json.toJson(promo))))
+    }.getOrElse[Future[Result]](Future.successful(BadRequest))
   }
 
   def upsert = Action.async { request =>
