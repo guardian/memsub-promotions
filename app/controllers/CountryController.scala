a1d93093a62d657a6f99dff0163e09d635e34435
tverran <tom.verran@guardian.co.uk>
2016-05-20 11:48:51 +0100

    Make UK mainland/overseas separate country groups

--- app/controllers/CountryController.scala
+++ app/controllers/CountryController.scala
@@ -1,7 +1,7 @@
 package controllers
 import com.gu.memsub.promo.Formatters.Common._
 import com.gu.config.{DigitalPackRatePlanIds, MembershipRatePlanIds}
-import com.gu.i18n.{Currency, Country, CountryGroup}
+import com.gu.i18n.{Country, CountryGroup, Currency, GBP}
 import play.api.libs.json._
 import play.api.mvc.Action
 import play.api.mvc.Results._
@@ -15,6 +15,14 @@ class CountryController(membershipIds: MembershipRatePlanIds, digipackIds: Digit
     }
 
     implicit val cgWrites = Json.writes[CountryGroup]
-    Ok(Json.toJson[List[CountryGroup]](CountryGroup.allGroups))
+
+    val overseasCountries = CountryGroup.UK.countries.filter(_ != Country.UK)
+
+    val mainlandUk = CountryGroup("UK mainland", "ukm", Some(Country.UK), List(Country.UK), CountryGroup.UK.currency)
+    val overseasUk = CountryGroup("UK overseas", "ukos", overseasCountries.headOption, overseasCountries, CountryGroup.UK.currency)
+
+    val countryGroups = List(mainlandUk, overseasUk) ++ CountryGroup.allGroups.filter(_ != CountryGroup.UK)
+
+    Ok(Json.toJson[List[CountryGroup]](countryGroups))
   }
 }
