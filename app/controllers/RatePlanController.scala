fdfc1ac518affba38bb7ec0ce0412a227bab7f89
Paul Brown <paul.brown@guardian.co.uk>
2016-05-24 10:58:33 +0100

    - Ordered the rate plans in a logical way - Layout tweaks to section colour selector, preview and save buttons - Tweaked toolbar colour for subs

--- app/controllers/RatePlanController.scala
+++ app/controllers/RatePlanController.scala
@@ -7,21 +7,26 @@ import play.api.mvc.Results._
 
 class RatePlanController(membershipIds: MembershipRatePlanIds, digipackIds: DigitalPackRatePlanIds) {
 
+  private def tupleToRatePlanMap(t: (String, String)) = {
+    Map("ratePlanId" -> t._1, "ratePlanName" -> t._2)
+  }
+
   def all = Action {
     Ok(Json.obj(
-      Membership.id -> Json.toJson(Map(
-      membershipIds.friend.get -> "Friend",
-      membershipIds.supporterMonthly.get -> "Supporter monthly",
-      membershipIds.supporterYearly.get -> "Supporter yearly",
-      membershipIds.partnerMonthly.get -> "Partner monthly",
-      membershipIds.partnerYearly.get -> "Partner yearly",
-      membershipIds.patronMonthly.get -> "Patron monthly",
-      membershipIds.patronYearly.get -> "Patron yearly"
-    )),
-    Digipack.id -> Json.toJson(Map(
-      digipackIds.digitalPackMonthly.get -> "Digital pack monthly",
-      digipackIds.digitalPackQuaterly.get -> "Digital pack quarterly",
-      digipackIds.digitalPackYearly.get -> "Digital pack yearly"
-    ))))
+      Membership.id -> Json.toJson(Seq(
+        membershipIds.friend.get -> "Friend",
+        membershipIds.supporterMonthly.get -> "Supporter monthly",
+        membershipIds.supporterYearly.get -> "Supporter yearly",
+        membershipIds.partnerMonthly.get -> "Partner monthly",
+        membershipIds.partnerYearly.get -> "Partner yearly",
+        membershipIds.patronMonthly.get -> "Patron monthly",
+        membershipIds.patronYearly.get -> "Patron yearly"
+      ).map(tupleToRatePlanMap)),
+      Digipack.id -> Json.toJson(Seq(
+        digipackIds.digitalPackMonthly.get -> "Digital pack monthly",
+        digipackIds.digitalPackQuaterly.get -> "Digital pack quarterly",
+        digipackIds.digitalPackYearly.get -> "Digital pack yearly"
+      ).map(tupleToRatePlanMap))
+    ))
   }
 }
