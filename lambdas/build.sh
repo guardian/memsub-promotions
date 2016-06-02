3ad776fd48cd24a088853fec7ccd5a31b302084a
Paul Brown <paul.brown@guardian.co.uk>
2016-06-02 10:07:35 +0100

    Updating lambda and build script to being just .js file and to not delete intermediary zip, so that the build script can be used locally

--- lambdas/build.sh
+++ lambdas/build.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
-cp MembershipSub-Promotions-to-PromoCode-View-Lambda.es6 MembershipSub-Promotions-to-PromoCode-View-Lambda.js
-zip -m MembershipSub-Promotions-to-PromoCode-View-Lambda.zip MembershipSub-Promotions-to-PromoCode-View-Lambda.js
+zip MembershipSub-Promotions-to-PromoCode-View-Lambda.zip MembershipSub-Promotions-to-PromoCode-View-Lambda.js
 mkdir -p packages/MembershipSub-Promotions-to-PromoCode-View-Lambda
-mv MembershipSub-Promotions-to-PromoCode-View-Lambda.zip packages/MembershipSub-Promotions-to-PromoCode-View-Lambda
+cp MembershipSub-Promotions-to-PromoCode-View-Lambda.zip packages/MembershipSub-Promotions-to-PromoCode-View-Lambda
 zip -r artifacts.zip packages deploy.json
 echo "##teamcity[publishArtifacts '$(pwd)/artifacts.zip']"
