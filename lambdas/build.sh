d9730827d8aab851886e3151b23dc3178e5a40e0
Paul Brown <paul.brown@guardian.co.uk>
2018-02-09 15:35:28 +0000

    Updated the Lambda build system.

--- lambdas/build.sh
+++ lambdas/build.sh
@@ -2,5 +2,5 @@
 zip MembershipSub-Promotions-to-PromoCode-View-Lambda.zip MembershipSub-Promotions-to-PromoCode-View-Lambda.js
 mkdir -p packages/MembershipSub-Promotions-to-PromoCode-View-Lambda
 cp MembershipSub-Promotions-to-PromoCode-View-Lambda.zip packages/MembershipSub-Promotions-to-PromoCode-View-Lambda
-zip -r artifacts.zip packages deploy.json
-echo "##teamcity[publishArtifacts '$(pwd)/artifacts.zip']"
+zip -r MembershipSub-Promotions-to-PromoCode-View-Lambda.zip.zip packages riff-raff.yaml
+echo "##teamcity[publishArtifacts '$(pwd)/MembershipSub-Promotions-to-PromoCode-View-Lambda.zip']"
