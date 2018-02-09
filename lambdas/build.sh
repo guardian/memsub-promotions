20db427ebcb827349a36809353ff3d9283e8047e
Paul Brown <paul.brown@guardian.co.uk>
2018-02-09 16:28:23 +0000

    I upgraded yarn which installs into a /yarn-v<version> folder rather than /dist. Therefore it doesn't need the parent version folder either.

--- lambdas/build.sh
+++ lambdas/build.sh
@@ -9,15 +9,15 @@ nvm use ${NODE_VERSION}
 
 # Installing yarn
 YARN_VERSION="1.3.2"
-YARN_LOCATION="tools/${YARN_VERSION}"
+YARN_LOCATION="tools"
 
 if [ ! -d "$YARN_LOCATION" ]; then
 	mkdir -p ${YARN_LOCATION}
 	cd ${YARN_LOCATION}/
 	wget -qO- https://github.com/yarnpkg/yarn/releases/download/v${YARN_VERSION}/yarn-v${YARN_VERSION}.tar.gz | tar zvx
-	cd ../..
+	cd ..
 fi
-export PATH="$PATH:${YARN_LOCATION}/dist/bin/"
+PATH="$PATH:${YARN_LOCATION}/yarn-v${YARN_VERSION}/bin/"
 
 # Installing packages via yarn
 
