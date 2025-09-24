c0d0a1d7276939681bc7c358180ad3211c55ef4b
Rupert Bates <Rupert.Bates@guardian.co.uk>
2025-09-24 14:16:50 +0100

    Remove all non 'plus' and 'voucher' rate plans from the `PaperProduct` class

--- app/conf/PaperPlans.scala
+++ app/conf/PaperPlans.scala
@@ -5,21 +5,15 @@ import wiring.AppComponents.Stage
 
 case class PaperProducts(
     digitalVoucher: PaperPlans,
-    voucher: PaperPlans,
     delivery: PaperPlans,
     nationalDelivery: NationalDeliveryPaperPlans,
 )
 
 case class PaperPlans(
-    saturday: ProductRatePlanId,
     saturdayplus: ProductRatePlanId,
     sunday: ProductRatePlanId,
-    sundayplus: ProductRatePlanId,
-    weekend: ProductRatePlanId,
     weekendplus: ProductRatePlanId,
-    sixday: ProductRatePlanId,
     sixdayplus: ProductRatePlanId,
-    everyday: ProductRatePlanId,
     everydayplus: ProductRatePlanId,
 )
 
@@ -32,15 +26,10 @@ case class NationalDeliveryPaperPlans(
 object PaperProducts {
 
   def plansFor(config: Config, product: String) = PaperPlans(
-    saturday = ProductRatePlanId(config.getString(s"$product.saturday")),
     saturdayplus = ProductRatePlanId(config.getString(s"$product.saturdayplus")),
     sunday = ProductRatePlanId(config.getString(s"$product.sunday")),
-    sundayplus = ProductRatePlanId(config.getString(s"$product.sundayplus")),
-    weekend = ProductRatePlanId(config.getString(s"$product.weekend")),
     weekendplus = ProductRatePlanId(config.getString(s"$product.weekendplus")),
-    sixday = ProductRatePlanId(config.getString(s"$product.sixday")),
     sixdayplus = ProductRatePlanId(config.getString(s"$product.sixdayplus")),
-    everyday = ProductRatePlanId(config.getString(s"$product.everyday")),
     everydayplus = ProductRatePlanId(config.getString(s"$product.everydayplus")),
   )
 
@@ -49,7 +38,6 @@ object PaperProducts {
 
     PaperProducts(
       digitalVoucher = plansFor(stageConfig, "digitalVoucher"),
-      voucher = plansFor(stageConfig, "voucher"),
       delivery = plansFor(stageConfig, "delivery"),
       nationalDelivery = NationalDeliveryPaperPlans(
         weekendplus = ProductRatePlanId(stageConfig.getString("nationalDelivery.weekendplus")),
